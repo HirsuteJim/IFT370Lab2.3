a98d47dec1a903f9a2cfc85ce19da74c73882ce0
Jim Termini <Jim.Termini@me.com>
2022-04-10 10:47:53 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿class Program {
+    static void Main(string[] args)   {
+        Singleton mySingleton = Singleton.Instance;
+        Console.WriteLine("\n\n================================================================================");
+        Console.WriteLine(mySingleton.GetThree());
+        Console.WriteLine("\n\n================================================================================");
+
+    }
+}
